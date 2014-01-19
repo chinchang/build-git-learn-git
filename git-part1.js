116871ab54376e489af538fa1ba1a7e04359e704
Kushagra Gour <chinchang457@gmail.com>
2014-01-19 20:00:27 +0530

    Add HEAD property and implement  `log()` function.

--- git-part1.js
+++ git-part1.js
@@ -5,10 +5,12 @@
 	 * A single commit.
 	 *
 	 * @param {number} id  		 ID of commit.
+	 * @param {Commit} parent	 Parent Commit.
 	 * @param {string} msg 		 Commit message.
 	 */
-	function Commit(id, message) {
+	function Commit(id, parent, message) {
 		this.id = id;
+		this.parent = parent;
 		this.message = message;
 	}
 
@@ -21,6 +23,7 @@
 	function Git(name) {
 		this.name = name; // Repo name
 		this.lastCommitId = -1; // Keep track of last commit id.
+		this.HEAD = null; // Reference to last Commit.
 	}
 
 	/**
@@ -30,10 +33,32 @@
 	 */
 	Git.prototype.commit = function (message) {
 		// Increment last commit id and pass into new commit.
-		var commit = new Commit(++this.lastCommitId, message);
+		var commit = new Commit(++this.lastCommitId, this.HEAD, message);
+
+		// Update HEAD and current branch.
+		this.HEAD = commit;
+
 		return commit;
+	};
+
+	/**
+	 * Logs history.
+	 * @return {Array} Commits in reverse chronological order.
+	 */
+	Git.prototype.log = function () {
+		// Start from HEAD
+		var commit = this.HEAD,
+			history = [];
+
+		while (commit) {
+			history.push(commit);
+			// Keep following the parent
+			commit = commit.parent;
+		}
+
+		return history;
 	};
 
 	// Expose Git class on window.
 	window.Git = Git;
-;})();
+})();
